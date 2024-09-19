9992f5fbb9fe31044a7a3553980e9764a79d022b
jh214 <sswed147@naver.com>
2024-09-19 19:33:46 +0900

    init : 초기 셋업

---
+++ src/main/java/techfair_comon/TechFairComonApplication.java
@@ -0,0 +1,13 @@
+package techfair_comon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechFairComonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechFairComonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/techfair_comon/TechFairComonApplicationTests.java
@@ -0,0 +1,13 @@
+package techfair_comon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechFairComonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
